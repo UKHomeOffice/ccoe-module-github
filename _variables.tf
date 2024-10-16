cd973f567347e6fbe6b1daf10b15ed1058524153
Mike Hosker <mike.hosker@homeoffice.gov.uk>
2024-10-16 23:46:14 +0100

    Add environments functionality to repos

--- _variables.tf
+++ _variables.tf
@@ -18,6 +18,17 @@ variable "repositories" {
       required_approving_review_count = optional(number, 0)
       require_code_owner_reviews      = optional(bool, false)
     })), {})
+    environments = optional(map(object({
+      name                    = string
+      can_admins_bypass       = optional(bool, false)
+      prevent_self_review     = optional(bool, false)
+      protected_branches_only = optional(bool, true)
+      custom_branch_policies  = optional(bool, false)
+      reviewers               = optional(object({
+        users = optional(list(string), []) # Either ID (number) OR GitHub username
+        teams = optional(list(string), []) # Either ID (number) OR key from team map OR GitHub team name
+      }), {})
+    })), {})
     allowed_actions = optional(list(string), [])
   }))
   description = "Repositories to create."
