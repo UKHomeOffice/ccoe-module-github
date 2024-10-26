8108967ba959872e8c602a497d4fad366d9a4407
Mike Hosker <mike.hosker@homeoffice.gov.uk>
2024-10-26 01:41:14 +0100

    Support for URL + read only protected branches (#3)

--- _variables.tf
+++ _variables.tf
@@ -10,6 +10,7 @@ variable "repositories" {
   type = map(object({
     name        = string
     description = string
+    url         = optional(string, null)
     visibility  = optional(string, "internal")
     archived    = optional(bool, false)
     protected_branches = optional(map(object({
@@ -17,6 +18,7 @@ variable "repositories" {
       checks                          = optional(list(string), [])
       required_approving_review_count = optional(number, 0)
       require_code_owner_reviews      = optional(bool, false)
+      read_only                       = optional(bool, false)
     })), {})
     environments = optional(map(object({
       name                    = string
@@ -24,7 +26,7 @@ variable "repositories" {
       prevent_self_review     = optional(bool, false)
       protected_branches_only = optional(bool, true)
       custom_branch_policies  = optional(bool, false)
-      reviewers               = optional(object({
+      reviewers = optional(object({
         users = optional(list(string), []) # Either ID (number) OR GitHub username
         teams = optional(list(string), []) # Either ID (number) OR key from team map OR GitHub team name
       }), {})
