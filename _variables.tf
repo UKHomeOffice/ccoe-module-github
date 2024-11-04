22706e3c401ea56c68d3262c52e0a00db4bb96e0
Mike Hosker <mike.hosker@homeoffice.gov.uk>
2024-11-04 18:15:42 +0000

    Support for use of repo projects

--- _variables.tf
+++ _variables.tf
@@ -8,11 +8,13 @@
 
 variable "repositories" {
   type = map(object({
-    name        = string
-    description = string
-    url         = optional(string, null)
-    visibility  = optional(string, "internal")
-    archived    = optional(bool, false)
+    name            = string
+    description     = string
+    url             = optional(string, null)
+    has_discussions = optional(bool, false)
+    has_wiki        = optional(bool, false)
+    visibility      = optional(string, "internal")
+    archived        = optional(bool, false)
     protected_branches = optional(map(object({
       pattern                         = string
       checks                          = optional(list(string), [])
@@ -30,6 +32,13 @@ variable "repositories" {
         users = optional(list(string), []) # Either ID (number) OR GitHub username
         teams = optional(list(string), []) # Either ID (number) OR key from team map OR GitHub team name
       }), {})
+    })), {})
+    projects = optional(map(object({
+      name = string
+      body = string
+      columns = map(object({
+        name = string
+      }))
     })), {})
     allowed_actions = optional(list(string), [])
   }))
--- repositories.tf
+++ repositories.tf
@@ -17,10 +17,10 @@ resource "github_repository" "repo" {
 
   delete_branch_on_merge = true
 
-  has_downloads = false
-  has_issues    = true
-  has_projects  = false
   has_wiki      = false
+  has_downloads   = false
+  has_issues      = true
+  has_projects    = length(each.value.projects) > 0 ? true : false
 
   vulnerability_alerts = true
 
@@ -129,4 +129,28 @@ resource "github_actions_repository_permissions" "allowed-actions" {
       "hashicorp/*",
     ], each.value.allowed_actions)
   }
+}
+
+# ---------------------------------------------------------
+# Projects
+# ---------------------------------------------------------
+
+locals {
+  projects = merge([for rkey, rval in var.repositories : { for pkey, pvalue in rval.projects : "${rkey}-${pkey}" => merge(pvalue, { repo = rkey }) }]...)
+  columns  = merge([ for pkey, pval in local.projects : { for ckey, cvalue in pval.columns : "${pkey}-${ckey}" => merge(cvalue, { project = pkey }) }]...)
+}
+
+resource "github_repository_project" "project" {
+  for_each = local.projects
+
+  name       = each.value.name
+  repository = github_repository.repo[each.value.repo].name
+  body       = each.value.body
+}
+
+resource "github_project_column" "column" {
+  for_each = local.columns
+
+  project_id = github_repository_project.project[each.value.project].id
+  name       = each.value.name
 }
